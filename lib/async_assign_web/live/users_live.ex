bb7eb3c1aac57724ac48f498dbfcbe98c0a87add
Johnny Curran <18315178+JohnnyCurran@users.noreply.github.com>
2023-08-31 17:38:21 -0500

    Showing off async assigns

--- lib/async_assign_web/live/users_live.ex
+++ lib/async_assign_web/live/users_live.ex
@@ -6,7 +6,42 @@ defmodule AsyncAssignWeb.UsersLive do
   @impl true
   def render(assigns) do
     ~H"""
-    i'm users live!
+    <h1 class="text-2xl font-semibold"><%= @table_header %></h1>
+
+    <span>Without the async assign helper:</span>
+    <div> Admin user: </div>
+    <div :if={@admin_user.loading}>Loading admin ...</div>
+    <div :if={admin_user = @admin_user.ok? && @admin_user.result}><%= admin_user.email %></div>
+
+    <div class="mt-8">With the async assign helper:</div>
+    <.async_result :let={user} assign={@admin_user}>
+      <:loading>Loading admin user...</:loading>
+      <:failed :let={reason}><%= reason %></:failed>
+      <%= if user do %>
+        <%= user.email %>
+      <% else %>
+        No admin user yet!
+      <% end %>
+    </.async_result>
+
+    <div class="mt-8">If something goes wrong:</div>
+    <.async_result :let={user} assign={@failed_to_load}>
+      <:loading>Loading admin user...</:loading>
+      <:failed :let={{:error, reason}}><%= reason %></:failed>
+      <%= if user do %>
+        <%= user.email %>
+      <% else %>
+        No admin user yet!
+      <% end %>
+    </.async_result>
+
+    <div class="container mx-auto mt-10">
+      <div class="flex flex-row font-bold">
+        <div class="flex-1 p-4 border">ID</div>
+        <div class="flex-1 p-4 border">Name</div>
+        <div class="flex-1 p-4 border">Email</div>
+      </div>
+    </div>
     """
   end
 
@@ -16,6 +51,7 @@ defmodule AsyncAssignWeb.UsersLive do
       socket
       |> assign(:table_header, "All Users")
       |> assign_async(:admin_user, fn -> {:ok, %{admin_user: Accounts.get_user_by_email("john@johnelmlabs.com")}} end)
+      |> assign_async(:failed_to_load, fn -> {:error, "Could not find user with email: nonexistent@johnelmlabs.com!"} end)
       |> assign_async([:john, :tim], fn -> {:ok, %{john: Accounts.get_user_by_email("john@johnelmlabs.com"), tim: Accounts.get_user_by_email("tim@johnelmlabs.com")}} end)
       |> assign_async(:all_users, fn -> {:ok, %{all_users: Accounts.list_users()}} end)}
   end
